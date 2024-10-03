1b432057a795145822eb00e697a944347b16364c
Suguru Ohashi <goothrough315@gmail.com>
2024-10-03 13:24:38 -0700

    finished on the 3rd pattern

---
+++ lab2_loops/Starry.py
@@ -0,0 +1,44 @@
+user_input = input("Enter the count of rows: ")
+# User input should always be integer value.
+n = int(user_input)
+
+# pattern1
+print("---pattern1---")
+max_stars = 2 * n - 1
+
+for i in range(1, n + 1):
+    current_stars = 2 * i - 1
+    for j in range((max_stars - current_stars) // 2):
+        print(" ", end="")
+    for k in range(current_stars):
+        print("*", end="")
+    for l in range((max_stars - current_stars) // 2):
+        print(" ", end="")
+    print()
+
+# pattern2
+print("---pattern2---")
+
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+# pattern3
+print("---pattern3---")
+
+for i in range(n // 2):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
+
+# Check if n is even
+if n % 2 != 0:
+    for i in range(n // 2 + 1):
+        print("*", end="")
+    print()
+
+for i in range(n // 2 - 1, -1, -1):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
