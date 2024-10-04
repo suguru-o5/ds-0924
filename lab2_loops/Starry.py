423a9b3c19038cac4d3228e39356ca4097117e8e
Suguru Ohashi <goothrough315@gmail.com>
2024-10-04 12:39:44 -0700

    add pattern 4 and 5

--- lab2_loops/Starry.py
+++ lab2_loops/Starry.py
@@ -4,6 +4,7 @@ n = int(user_input)
 
 # pattern1
 print("---pattern1---")
+
 max_stars = 2 * n - 1
 
 for i in range(1, n + 1):
@@ -41,4 +42,61 @@ if n % 2 != 0:
 for i in range(n // 2 - 1, -1, -1):
     for j in range(i + 1):
         print("*", end="")
+    print()
+
+# pattern4
+print("---pattern4---")
+
+for i in range(n // 2):
+    for j in range(i):
+        print(" ", end="")
+    for k in range(n - 2 * i):
+        print("*", end="")
+    for l in range(i):
+        print(" ", end="")
+    print()
+# Check if n is even
+if n % 2 != 0:
+    for i in range((n - 1) // 2):
+        print(" ", end="")
+    print("*", end="")
+    for i in range((n - 1) // 2):
+        print(" ", end="")
+    print()
+for i in range(n // 2 - 1, -1, -1):
+    for j in range(i):
+        print(" ", end="")
+    for k in range(n - 2 * i):
+        print("*", end="")
+    for l in range(i):
+        print(" ", end="")
+    print()
+
+# pattern5
+print("---pattern5---")
+
+for i in range(n // 2, 0, -1):
+    if n - 2 * i == 0:
+        continue
+    for j in range(i):
+        print(" ", end="")
+    for k in range(n - 2 * i):
+        print("*", end="")
+    for l in range(i):
+        print(" ", end="")
+    print()
+# Check if n is even
+if n % 2 != 0:
+    for i in range(n):
+        print("*", end="")
+    print()
+for i in range(1, n // 2 + 1):
+    if n - 2 * i == 0:
+        continue
+    for j in range(i):
+        print(" ", end="")
+    for k in range(n - 2 * i):
+        print("*", end="")
+    for l in range(i):
+        print(" ", end="")
     print()
