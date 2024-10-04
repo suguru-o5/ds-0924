9967b2ac089bbb82be3f4ce386940432637e58dc
Suguru Ohashi <goothrough315@gmail.com>
2024-10-04 13:03:11 -0700

    add some explainations

--- lab2_loops/Part1.py
+++ lab2_loops/Part1.py
@@ -2,6 +2,8 @@ user_input_num = input("Enter a number: ")
 user_input_n = input("Enter n: ")
 
 # User input should always be integer value.
+# If the user inputs 0 into n, 
+# this program outputs the whole num because the nth digit does not exist.
 num = int(user_input_num)
 n = int(user_input_n)
 
