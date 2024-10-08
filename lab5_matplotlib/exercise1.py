cfb3bd29514c4140a78ccab81939d9d56b86d23e
Suguru Ohashi <goothrough315@gmail.com>
2024-10-08 13:28:16 -0700

    lab5 exercise1

---
+++ lab5_matplotlib/exercise1.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [0, 1, 2, 3, 4, 5, 6]
+y = [0, 1, 4, 9, 16, 25, 36]
+
+plt.plot(x,y)
+plt.xlabel("Example-x-label")
+plt.ylabel("Example-y-label")
+plt.title("Example-title")
+plt.show()
