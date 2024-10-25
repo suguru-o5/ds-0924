280e820661c1452a48d594b96c8f40dab7946ef3
Suguru Ohashi <goothrough315@gmail.com>
2024-10-25 14:57:10 -0700

    logic fixed

--- mini_project3_hangman/hangman.py
+++ mini_project3_hangman/hangman.py
@@ -1,75 +1,142 @@
 import random
 
+level_to_word_list = {
+    "easy": [
+        "cat",
+        "dog",
+        "sun",
+        "book",
+        "fish",
+        "tree",
+        "chair",
+        "house",
+        "apple",
+        "ball",
+    ],
+    "medium": [
+        "engine",
+        "window",
+        "bottle",
+        "garden",
+        "pirate",
+        "candle",
+        "basket",
+        "winter",
+        "bridge",
+        "monster",
+    ],
+    "hard": [
+        "astronomy",
+        "architecture",
+        "knowledge",
+        "revolution",
+        "unbelievable",
+        "adventure",
+        "questionnaire",
+        "persistence",
+        "trampoline",
+        "hypothesis",
+    ],
+}
+
 
 def main():
+    display_menu()
+
     answer = prepare_answer()
-    incorrect_guesses_remaining = prepare_incorrect_guesses_remaining(answer)
-
-    print("Welcome to Hangman!")
-
-    status_dict = prepare_status_dict(answer)
-
+    incorrect_guesses_remaining = prepare_incorrect_guesses_remaining()
+    processing_letter_list = prepare_processing_letter_list(answer)
+    usedletter_to_index = prepare_usedletter_to_index(answer)
     guessed_letter_list = []
 
     while True:
         # Game over logic
         if incorrect_guesses_remaining == 0:
-            print(f"Game over! The word was: {answer}")
+            print(f"Game over! The word was: '{answer}'")
             break
         # Game clear logic
-        if not False in status_dict.values():
-            print(f"Congratulations! You guessed the word: {answer}")
+        if not any(usedletter_to_index):
+            print(f"Congratulations! You guessed the word: '{answer}'")
             break
 
         # Print current word
         print("Current word: ", end="")
-        for letter in list(answer):
-            print_letter = letter if status_dict.get(letter) else "_"
-            print(print_letter, end="")
-            print(" ", end="")
+        for letter in processing_letter_list:
+            print(letter, end=" ")
         print("")
 
         # Print guessed letters
         print("Guessed letters: ", end="")
         for letter in guessed_letter_list:
-            print(letter, end="")
-            print(", ", end="")
+            print(letter, end=", ")
         print("")
 
+        # Print incorrect guess remaining
         print(f"Incorrect guesses remaining: {incorrect_guesses_remaining}")
 
         input_letter = validated_input_letter(guessed_letter_list)
         guessed_letter_list.append(input_letter)
 
-        if status_dict.get(input_letter) is None:
-            print(f"Sorry, {input_letter} is not in the word.")
+        if usedletter_to_index.get(input_letter) is None:
+            print(f"Sorry, '{input_letter}' is not in the word.")
             print("")
             incorrect_guesses_remaining -= 1
         else:
-            print(f"Good job! {input_letter} is in the word.")
+            print(f"Good job! '{input_letter}' is in the word.")
             print("")
-            status_dict[input_letter] = True
+
+            # Replace the blank letter in the processing letter list
+            # with the guessed letter
+            for to_be_replaced_index in usedletter_to_index[input_letter]:
+                processing_letter_list[to_be_replaced_index] = input_letter
+            del usedletter_to_index[input_letter]
+
+
+def display_menu():
+    print("Welcome to Hangman!")
+    print("")
+
+
+def determine_game_level():
+    while True:
+        print("Select the game level")
+        print("1. Easy")
+        print("2. Medium")
+        print("3. Hard")
+        input_game_level = input("Input the level by number: ")
+        level_definition = {"1": "easy", "2": "medium", "3": "hard"}
+
+        if not level_definition.get(input_game_level):
+            print("Please input 1, 2, or 3")
+            print("")
+            continue
+        else:
+            return level_definition[input_game_level]
 
 
 def prepare_answer():
-    words = ["python", "java", "go", "typescript", "php", "javascript"]
-
-    random_int = random.randrange(0, len(words) - 1)
-    return words[random_int]
-
-
-def prepare_incorrect_guesses_remaining(answer: str):
-    return len(answer)
+    game_level = determine_game_level()
+    word_list = level_to_word_list[game_level]
+    return random.choice(word_list)
 
 
-def prepare_status_dict(answer: str):
-    letter_list = list(answer)
-    status_dict = {}
+def prepare_incorrect_guesses_remaining():
+    # Traditionally, 8 is common because a hangman can be drawn with eight parts.
+    return 8
 
-    for char in letter_list:
-        status_dict[char] = False
 
-    return status_dict
+def prepare_processing_letter_list(answer: str):
+    return ["_"] * len(answer)
+
+
+def prepare_usedletter_to_index(answer: str):
+    usedletter_to_index: dict = {}
+    for i in range(len(answer)):
+        if usedletter_to_index.get(answer[i]):
+            usedletter_to_index[answer[i]].append(i)
+        else:
+            usedletter_to_index[answer[i]] = [i]
+    return usedletter_to_index
 
 
 def validated_input_letter(guessed_letter_list: list):
